f0858dfe0c4fd3cb135d33913ae42cef64128e74
Frank <user@Franks-MacBook-Pro.local>
2015-06-15 16:13:02 +1000

    Implement slippery with dummy data
    This may one works, but only for the dummy data. The payment can be
    made successfully to our account. Design will be changed later

--- Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/AlipayActivityFragment.java
+++ Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/AlipayActivityFragment.java
@@ -5,6 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+import android.util.*;
+import com.alipay.sdk.app.PayTask;
 
 import tripalocal.com.au.tripalocalbeta.R;
 
@@ -21,4 +37,7 @@ public class AlipayActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_alipay, container, false);
     }
+
+
+
 }
