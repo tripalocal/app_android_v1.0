1fc38bf847fee2993f67d38e173bcc227c3144eb
Frank <user@Franks-MBP.netgear.com>
2015-06-24 16:15:30 +1000

    add check first time(testing now),stuff remaining
    tasks remaining, remove actionBar, change the link of login point to
    the real page.

--- Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/PhoneregisterActivity.java
+++ Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/PhoneregisterActivity.java
@@ -1,6 +1,7 @@
 package tripalocal.com.au.tripalocalbeta.Views;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,7 +14,13 @@ public class PhoneregisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getActionBar().hide();
         setContentView(R.layout.activity_phoneregister);
+//        if(checkFirstTime()){
+//            Intent intent=new Intent(this,HomeActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intent);
+//        }
 
     }
 
@@ -27,5 +34,19 @@ public class PhoneregisterActivity extends AppCompatActivity {
         Intent intent =new Intent(this, HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    public Boolean checkFirstTime(){
+        SharedPreferences prefs = getSharedPreferences("Baisc", MODE_PRIVATE);
+        String restoredText = prefs.getString("firsttime", null);
+        if (restoredText == null) {
+            SharedPreferences.Editor editor = getSharedPreferences("Basic", MODE_PRIVATE).edit();
+            editor.putString("firsttime", "yes");
+            editor.commit();
+            return false;
+        }else{
+            return true;
+        }
+
     }
 }
