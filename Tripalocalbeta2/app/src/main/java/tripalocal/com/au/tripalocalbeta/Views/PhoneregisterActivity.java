f2a1b81a111075e995c1b0dcbd066e69cf26b0f9
Frank <user@Franks-MacBook-Pro.local>
2015-06-27 19:48:22 +1000

    Ignore the phone registration page If user already logged in

--- Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/PhoneregisterActivity.java
+++ Tripalocalbeta2/app/src/main/java/tripalocal/com/au/tripalocalbeta/Views/PhoneregisterActivity.java
@@ -1,5 +1,6 @@
 package tripalocal.com.au.tripalocalbeta.Views;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,11 +12,18 @@ import tripalocal.com.au.tripalocalbeta.R;
 //import android
 
 public class PhoneregisterActivity extends AppCompatActivity {
+    public static final String PREFS_NAME_L = "TPPrefs_L";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
+        SharedPreferences settings_l = getSharedPreferences(PREFS_NAME_L, Context.MODE_PRIVATE);
+        if(settings_l.getBoolean("login", false)){
+            Intent intent =new Intent(this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
 //        getActionBar().hide();
         setContentView(R.layout.activity_phoneregister);
 //        if(checkFirstTime()){
