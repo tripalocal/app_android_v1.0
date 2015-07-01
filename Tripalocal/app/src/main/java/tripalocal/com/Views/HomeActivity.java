c46c4226ec0f031945d965ebfd6c04befffb9979
naveen ravi <navin27in@gmail.com>
2015-07-01 22:17:29 +1000

    das

--- Tripalocal/app/src/main/java/tripalocal/com/Views/HomeActivity.java
+++ Tripalocal/app/src/main/java/tripalocal/com/Views/HomeActivity.java
@@ -263,7 +263,7 @@ public class HomeActivity extends AppCompatActivity {
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
-        searchView.setQueryHint("Test Search..");
+        searchView.setQueryHint(getResources().getString(R.string.search_hint));
         return searchView;
     }
 
