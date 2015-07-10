2f0328ebfa20ec4d5d5119e2629e46b7fabd1b63
Frankcf123 <chenfang1993cf135@gmail.com>
2015-07-10 13:37:22 +1000

    add visual back button for message activity

--- Tripalocal/app/src/main/java/com/tripalocal/bentuke/Views/ChatActivity.java
+++ Tripalocal/app/src/main/java/com/tripalocal/bentuke/Views/ChatActivity.java
@@ -4,14 +4,20 @@ import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.tripalocal.bentuke.R;
+import com.tripalocal.bentuke.adapters.ExperienceListAdapter;
+import com.tripalocal.bentuke.adapters.TPSuggestionsAdapter;
 import com.tripalocal.bentuke.helpers.ToastHelper;
 import com.umeng.analytics.MobclickAgent;
 
@@ -25,44 +31,56 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_chat);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_login) {
-            if (HomeActivity.getCurrent_user().isLoggedin()) {
-                HomeActivity.getCurrent_user().setLogin_token(null);
-                HomeActivity.getCurrent_user().setLoggedin(false);
-                HomeActivity.setAccessToken(null);
-                SharedPreferences settings_l = getSharedPreferences(HomeActivity.PREFS_NAME_L, Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor_l = settings_l.edit();
-                editor_l.clear();
-                editor_l.apply();
-                ToastHelper.shortToast(getResources().getString(R.string.logged_out));
-            } else
-                getSupportFragmentManager().beginTransaction().replace(R.id.payment_content, new LoginFragment()).commit();
-            return true;
-        }
+       finish();
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
         finish();
-//        super.onBackPressed();
+        super.onBackPressed();
 
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_msg_detail, menu);
+//        if(menu_ref == null)
+//            menu_ref= menu;
+//        if(getCurrent_user().isLoggedin()){
+//            menu.findItem(R.id.action_login).setTitle(getResources().getString(R.string.logout));
+//        }
+//
+//        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+//            @Override
+//            public boolean onSuggestionSelect(int position) {
+//                //ToastHelper.shortToast("sugg select "+position);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onSuggestionClick(int position) {
+//                //ToastHelper.shortToast("sugg click "+position +" : "+ db_poi_data[position]);
+//                Fragment exp_list_frag = new ExperiencesListFragment();
+//                Bundle args = new Bundle();
+//                args.putInt(ExperienceListAdapter.INT_EXTRA, position);
+//                exp_list_frag.setArguments(args);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, exp_list_frag).addToBackStack("home_sugg").commit();
+//                return false;
+//            }
+//        });
         return true;
     }
+
+
+
     public void onResume() {
         super.onResume();
         MobclickAgent.onResume(this);       //统计时长
